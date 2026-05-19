f87d40aa6831db02168387e73672de41a5536f99
RemydreScarlet <remydre8@gmail.com>
2026-05-19 21:30:13 +0900

    test: update TaskQueue test to use environment for instantiation

--- packages/worker/src/worker/__tests__/TaskQueue.test.ts
+++ packages/worker/src/worker/__tests__/TaskQueue.test.ts
@@ -1,9 +1,11 @@
 import { describe, it, expect } from 'vitest';
+import { env } from 'cloudflare:test';
 import { TaskQueue } from '../TaskQueue';
 
 describe('TaskQueue', () => {
-  it('should be instantiable', () => {
-    const queue = new TaskQueue();
+  it('should be instantiable', async () => {
+    const id = env.TASK_QUEUE.idFromName('test-queue');
+    const queue = env.TASK_QUEUE.get(id);
     expect(queue).toBeDefined();
   });
 });
